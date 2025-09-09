ee3ca48f4317bfca032880e506a42d2e0d1265ab
Jainkarn Pitakkaittikool <jainkarnp@ocean.com.au>
2025-09-09 15:20:58 +1000

    adjust margin

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -78,7 +78,7 @@ export default function ContainerComponent() {
                   <Grid item xs={12} sx={{ textAlign: 'center' }}>
                       <Typography variant="h4" sx={{ margin: '1rem 0' }}>Input Screen</Typography>
                   </Grid>
-                  <Grid item md={12} sx={{ textAlign: 'center', marginBottom: '5rem' }} className="money-display-div">
+                  <Grid item md={12} sx={{ textAlign: 'center', marginBottom: '2rem' }} className="money-display-div">
                     <MoneyDisplay money={money} handleCancelPayment={onCancelPayment} />
                   </Grid>
                 </Grid>
